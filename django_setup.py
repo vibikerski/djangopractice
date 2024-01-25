68ae5d1f2b40bb7502cbdd5c89fd52ced1abc3d0
vibikerski <vibikerski@gmail.com>
2024-01-25 20:24:18 +0100

    Game management system

---
+++ django_setup.py
@@ -0,0 +1,6 @@
+import sys
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangopractice.settings")
+django.setup()
--- lesson/models.py
+++ lesson/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 
-class User(models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=100)
-    bio = models.TextField()
-    email = models.EmailField()
-    birthday = models.DateField()
+
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    name = models.CharField(max_length=150)
+    creation_year = models.IntegerField()
+    rating = models.DecimalField(max_digits=4, decimal_places=2)
+
+    genre = models.ManyToManyField(Genre, related_name="games")
+
+
+    def __str__(self):
+        return self.name
